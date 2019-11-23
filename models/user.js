d8f40eb02606fac894e8848c8ce400863948ea1b
Shawn <pan.conan@gmail.com>
2019-11-23 22:18:55 +0800

    add register feature

--- models/user.js
+++ models/user.js
@@ -7,11 +7,11 @@ const userSchema = new Schema({
         type:String
     },
     email: {
-        type: String
+        type: String,
         required: true
     },
     password: {
-        type: String
+        type: String,
         required: true
     },
     date: {
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user.js')
 
 router.get('/login', (req, res) => {
     res.render('login')
@@ -14,7 +15,31 @@ router.get('/register', (req, res)=> {
 })
 
 router.post('/register', (req, res) => {
-
+    const {name, email, password, password2} = req.body
+    User.findOne({email: email}).then(user => {
+        // 如果使用者存在，就回到註冊頁
+        if (user) {
+            console.log('使用者存在')
+            res.render('register',{
+                name,
+                email,
+                password,
+                password2
+            })
+        } else {   // 如果使用者不存在，就儲存到資料庫
+            const newUser = new User({
+                name,
+                email,
+                password
+            })
+            newUser
+                .save()
+                .then(user => {
+                    res.redirect('/')
+                })
+                .catch(err => console.log(err))
+        }
+    })
 })
 
 router.post('/logout', (req, res) => {
